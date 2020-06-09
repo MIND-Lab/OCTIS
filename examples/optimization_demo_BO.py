fb4232d8e6ec6aa447da40194778d6fa51aea41d
Davide Pietrasanta <davide.pietrasanta@virgilio.it>
2020-06-09 17:52:16 +0200

    Update

--- examples/optimization_demo_BO.py
+++ examples/optimization_demo_BO.py
@@ -3,37 +3,49 @@
 
 from models.LDA import LDA_Model
 from dataset.dataset import Dataset
-from evaluation_metrics.diversity_metrics import Topic_diversity
+from evaluation_metrics.coherence_metrics import Coherence_word_embeddings_pairwise, Coherence_word_embeddings_centroid
 from evaluation_metrics.topic_significance_metrics import KL_uniform
 from optimization.optimizer import Optimizer
+from optimization.optimizer import default_parameters as BO_parameters
 from skopt.space.space import Real, Integer
 from skopt import gp_minimize, forest_minimize, dummy_minimize
 import multiprocessing as mp
 from gensim.models import Word2Vec
-
+import time
+    
 # Load dataset
 dataset = Dataset()
 dataset.load("preprocessed_datasets/newsgroup/newsgroup_lemmatized_10")
-
+    
 # Load model
 model = LDA_Model()
 
 # Set model hyperparameters
-model.hyperparameters['num_topics'] = 20
+model.hyperparameters.update({'num_topics':20})
 
-# Define metrics
-topic_diversity = Topic_diversity()
-kl_uniform = KL_uniform()
+# Coherence word embeddings pairwise
+metric_params = {
+    'topk':10,
+    'w2v_model': Word2Vec(dataset.get_corpus())
+}
+c_we_p = Coherence_word_embeddings_pairwise(metric_params)
+
+# Coherence word embeddings pairwise
+c_we_c = Coherence_word_embeddings_centroid(metric_params)
 
 # Define optimization parameters
 opt_params = {}
-opt_params["n_calls"] = 30
-opt_params["minimizer"] = forest_minimize
-opt_params["different_iteration"] = 3
-opt_params["n_random_starts"] = 5
-opt_params["extra_metrics"] = [kl_uniform] # List of extra metrics
-opt_params["n_jobs"] = mp.cpu_count() -1 # Enable multiprocessing
-opt_params["verbose"] = True
+opt_params["n_calls"] = 40
+opt_params["minimizer"] = gp_minimize
+opt_params["different_iteration"] = 5
+opt_params["n_random_starts"] = 4 
+opt_params["extra_metrics"] = [c_we_p] # List of extra metrics
+opt_params["n_jobs"] = mp.cpu_count() # Enable multiprocessing, if -1 do the same
+opt_params["save"] = True
+opt_params["save_step"] = 1
+opt_params["save_path"] = None #"risultati/giacomo/" 
+opt_params["early_stop"] = True
+opt_params["plot"] = True
 
 # Create search space for optimization
 search_space = {
@@ -45,22 +57,28 @@ search_space = {
 optimizer = Optimizer(
     model,
     dataset,
-    topic_diversity,
+    c_we_c,
     search_space,
     opt_params)
 
 # Disable computing of topic document matrix to optimize performance
 optimizer.topic_document_matrix = False
+optimizer.topic_word_matrix = False
 
 # Optimize
+start_time = time.time()
 res = optimizer.optimize()
+end_time = time.time()
+total_time = end_time - start_time
+
 
 print(res.hyperparameters) # Best values for the hyperparameters
 print(res.function_values) # Score of the optimized metric
 print("Optimized metric: "+res.optimized_metric)
+print("%s seconds" % (total_time))
 
-res.save(name ="Result", path = "optimization")
-    
+stringa_parameters = str(BO_parameters) + "\nTime: " + str(total_time) + " seconds"
 
-# Plot data NOT WORKING
-#res.plot_all(metric="Coherence_word_embeddings_pairwise")
+res.save(name ="Result", path = "risultati/giacomo/", parameters = stringa_parameters)
+
+#res.plot
