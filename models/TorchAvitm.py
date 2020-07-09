fe23ec5b74a36445680305585c70a4477fc79e2d
anonymous emnlp2020 <s.terragni4@campus.unimib.it>
2020-07-09 10:00:26 +0200

    setting of the hyperparameters for avitm fixed

--- models/TorchAvitm.py
+++ models/TorchAvitm.py
@@ -9,59 +9,78 @@ from models.pytorchavitm import datasets
 
 
 class TorchAvitm(Abstract_Model):
-    """
-    Args
-        dataset: list of sentences for training the model
-        hyparameters: dict, with the below information: 
-        
-        input_size : int, dimension of input
-        n_components : int, number of topic components, (default 10)
-        model_type : string, 'prodLDA' or 'LDA' (default 'prodLDA')
-        hidden_sizes : tuple, length = n_layers, (default (100, 100))
-        activation : string, 'softplus', 'relu', (default 'softplus')
-        dropout : float, dropout to use (default 0.2)
-        learn_priors : bool, make priors a learnable parameter (default True)
-        batch_size : int, size of batch to use for training (default 64)
-        lr : float, learning rate to use for training (default 2e-3)
-        momentum : float, momentum to use for training (default 0.99)
-        solver : string, optimizer 'adam' or 'sgd' (default 'adam')
-        num_epochs : int, number of epochs to train for, (default 100)
-        reduce_on_plateau : bool, reduce learning rate by 10x on plateau of 10 epochs (default False)
-    """
+
+    def __init__(self):
+        self.hyperparameters={}
 
     def train_model(self, dataset, hyperparameters, top_words=10, topic_word_matrix=True, topic_document_matrix=True):
+        """
+            Args
+                dataset: list of sentences for training the model
+                hyparameters: dict, with the below information:
 
-        self.n_components = hyperparameters.get('n_components', 10)
-        self.model_type = hyperparameters.get('model_type', 'prodLDA')
-        self.hidden_sizes = hyperparameters.get('hidden_sizes', (100,100))
-        self.activation = hyperparameters.get('activation', 'softplus')
-        self.dropout = hyperparameters.get('dropout', 0.2)
-        self.learn_priors = hyperparameters.get('learn_priors', True)
-        self.batch_size = hyperparameters.get('batch_size', 64)
-        self.lr = hyperparameters.get('lr', 2e-3)
-        self.momentum = hyperparameters.get('momentum', 0.99)
-        self.solver = hyperparameters.get('solver', 'adam')
-        self.num_epochs = hyperparameters.get('num_epochs', 100)
-        self.reduce_on_plateau = hyperparameters.get('reduce_on_plateau', False)
-        
+                input_size : int, dimension of input
+                n_components : int, number of topic components, (default 10)
+                model_type : string, 'prodLDA' or 'LDA' (default 'prodLDA')
+                hidden_sizes : tuple, length = n_layers, (default (100, 100))
+                activation : string, 'softplus', 'relu', (default 'softplus')
+                dropout : float, dropout to use (default 0.2)
+                learn_priors : bool, make priors a learnable parameter (default True)
+                batch_size : int, size of batch to use for training (default 64)
+                lr : float, learning rate to use for training (default 2e-3)
+                momentum : float, momentum to use for training (default 0.99)
+                solver : string, optimizer 'adam' or 'sgd' (default 'adam')
+                num_epochs : int, number of epochs to train for, (default 100)
+                reduce_on_plateau : bool, reduce learning rate by 10x on plateau of 10 epochs (default False)
+            """
+
+        self.set_default_hyperparameters(hyperparameters)
+
         data_corpus = [','.join(i) for i in dataset.get_corpus()]
         
-        self.X_train, self.input_size = self.preprocess(data_corpus)
+        X_train, input_size = self.preprocess(data_corpus)
       
-        self.avitm_model = avitm.AVITM(input_size=self.input_size, n_components=self.n_components,
-                                  model_type=self.model_type,hidden_sizes=self.hidden_sizes, 
-                                  activation=self.activation, dropout=self.dropout,
-                                  learn_priors=self.learn_priors, batch_size=self.batch_size, 
-                                  lr=self.lr, momentum=self.momentum,
-                                  solver=self.solver, num_epochs=self.num_epochs, 
-                                  reduce_on_plateau=self.reduce_on_plateau)
+        avitm_model = avitm.AVITM(input_size=input_size, n_components=self.hyperparameters['n_components'],
+                                  model_type=self.hyperparameters['model_type'],
+                                  hidden_sizes=self.hyperparameters['hidden_sizes'],
+                                  activation=self.hyperparameters['activation'],
+                                  dropout=self.hyperparameters['dropout'],
+                                  learn_priors=self.hyperparameters['learn_priors'],
+                                  batch_size=self.hyperparameters['batch_size'],
+                                  lr=self.hyperparameters['lr'],
+                                  momentum=self.hyperparameters['momentum'],
+                                  solver=self.hyperparameters['solver'],
+                                  num_epochs=self.hyperparameters['num_epochs'],
+                                  reduce_on_plateau=self.hyperparameters['reduce_on_plateau'])
     
-        self.avitm_model.fit(self.X_train)
+        avitm_model.fit(X_train)
         result = self.avitm_model.get_info()
         
-        return result 
+        return result
 
-    
+    def set_default_hyperparameters(self, hyperparameters):
+        self.hyperparameters['n_components'] = hyperparameters.get('n_components',
+                                                                   self.hyperparameters.get('n_components', 10))
+        self.hyperparameters['model_type'] = hyperparameters.get('model_type',
+                                                                 self.hyperparameters.get('model_type', 'prodLDA'))
+        self.hyperparameters['hidden_sizes'] = hyperparameters.get('hidden_sizes',
+                                                                   self.hyperparameters.get('hidden_sizes', (100, 100)))
+        self.hyperparameters['activation'] = hyperparameters.get('activation',
+                                                                 self.hyperparameters.get('activation', 'softplus'))
+        self.hyperparameters['dropout'] = hyperparameters.get('dropout', self.hyperparameters.get('dropout', 0.2))
+        self.hyperparameters['learn_priors'] = hyperparameters.get('learn_priors',
+                                                                   self.hyperparameters.get('learn_priors', True))
+        self.hyperparameters['batch_size'] = hyperparameters.get('batch_size',
+                                                                 self.hyperparameters.get('batch_size', 64))
+        self.hyperparameters['lr'] = hyperparameters.get('lr', self.hyperparameters.get('lr', 2e-3))
+        self.hyperparameters['momentum'] = hyperparameters.get('momentum', self.hyperparameters.get('momentum', 0.99))
+        self.hyperparameters['solver'] = hyperparameters.get('solver', self.hyperparameters.get('solver', 'adam'))
+        self.hyperparameters['num_epochs'] = hyperparameters.get('num_epochs',
+                                                                 self.hyperparameters.get('num_epochs', 100))
+        self.hyperparameters['reduce_on_plateau'] = hyperparameters.get('reduce_on_plateau',
+                                                                        self.hyperparameters.get('reduce_on_plateau',
+                                                                                                 False))
+
     @staticmethod
     def preprocess(data):
         
