a73f5d371816774e751e4a0e610b51ac3f2c01b2
anonymous emnlp2020 <s.terragni4@campus.unimib.it>
2020-08-03 14:08:01 +0200

    fixed typo

--- examples/preprocessing_demo.py
+++ examples/preprocessing_demo.py
@@ -9,10 +9,11 @@ import multiprocessing as mp
 dataset = source.retrieve()
 
 # All parameters defaults are True
-pipeline_handler = Pipeline_handler(dataset,num_processes=mp.cpu_count(), display_progress=True,
+pipeline_handler = Pipeline_handler(dataset, num_processes=mp.cpu_count(), display_progress=True,
                                     min_words_for_doc=2, words_min_freq=0.01,
-                                    words_max_freq=0.9, stopwords='english',)
+                                    words_max_freq=1, stopwords='english')
 
 preprocessed = pipeline_handler.preprocess()
 
 preprocessed.save("dataset_folder")
+
