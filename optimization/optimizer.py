cebad58fabaed4c88455380d3722e128210a732c
brunoG89 <61013653+brunoG89@users.noreply.github.com>
2020-08-25 21:57:52 +0200

    bug fixed related to mkdir

--- optimization/optimizer.py
+++ optimization/optimizer.py
@@ -83,7 +83,7 @@ class Optimizer:
         self.optimization_type = default_parameters['optimization_type']
 
         if default_parameters["save_path"] is not None:
-            Path(default_parameters["save_path"]).mkdir(exist_ok=True)      
+            Path(default_parameters["save_path"]).mkdir(parents=True, exist_ok=True)      
         
         if ((default_parameters["save_models"] == True) and (default_parameters["save_path"] is not None)):
             model_path = default_parameters["save_path"] + "models/"
