95917ce22af704aaf958e77ae90d688e4bd5bdd9
anonymous emnlp2020 <s.terragni4@campus.unimib.it>
2020-11-27 16:01:35 +0100

    updated requirements and refactoring

---
+++ optopic/dashboard/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from multiprocessing import Process, Pool
+import optopic.dashboard.frameworkScanner as fs
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/CreateExperiments')
+def CreateExperiments():
+    datasets = fs.scanDatasets()
+    return render_template("CreateExperiments.html", datasets=datasets)
+
+
+@app.route('/VisualizeExperiments')
+def VisualizeExperiments():
+    return render_template("VisualizeExperiments.html")
+
+
+@app.route('/ManageExperiments')
+def ManageExperiments():
+    return render_template("ManageExperiments.html")
