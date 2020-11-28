6cef7b8dd770bc33fe2b3cd17738d974368e0e20
pietrotrope <pi.tropeano@gmail.com>
2020-11-28 19:28:59 +0100

    Added model selection (to do hyperparameters)

--- optopic/dashboard/server.py
+++ optopic/dashboard/server.py
@@ -12,15 +12,47 @@ def home():
 
 @app.route('/CreateExperiments')
 def CreateExperiments():
+    models = {
+        "LDA": {"alpha": {
+            "type": "real",
+            "default_value": 0.1,
+            "min_value": 1e-4,
+            "max_value": 20},
+            "beta": {
+            "type": "real",
+            "default_value": 0.1,
+            "min_value": 1e-4,
+            "max_value": 20}},
+        "NMF": {"pizza": {
+            "type": "categorical",
+            "default_value": "margherita",
+            "possible_values": ["kebab", "zola e noci", "4 formaggi"]
+        }},
+        "HDP": {"alpha": {
+            "type": "integer",
+            "default_value": 0,
+            "min_value": 0,
+            "max_value": 20},
+            "beta": {
+            "type": "real",
+            "default_value": 0.1,
+            "min_value": 1e-4,
+            "max_value": 20},
+            "pizza": {
+            "type": "categorical",
+            "default_value": "margherita",
+            "possible_values": ["kebab", "zola e noci", "4 formaggi"]
+        }}
+    }
     datasets = fs.scanDatasets()
-    return render_template("CreateExperiments.html", datasets=datasets)
+    return render_template("CreateExperiments.html", datasets=datasets, models=models)
 
 
-@app.route('/VisualizeExperiments')
+@ app.route('/VisualizeExperiments')
 def VisualizeExperiments():
     return render_template("VisualizeExperiments.html")
 
 
-@app.route('/ManageExperiments')
+@ app.route('/ManageExperiments')
 def ManageExperiments():
     return render_template("ManageExperiments.html")
