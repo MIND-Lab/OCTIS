565e2fd4dab0b459eb250ed209d08287198f61ae
anonymous emnlp2020 <s.terragni4@campus.unimib.it>
2020-12-09 12:05:05 +0100

    fixed tests hopefully

--- tests/test_optopic.py
+++ tests/test_optopic.py
@@ -23,36 +23,10 @@ def root_dir():
 def data_dir(root_dir):
     return root_dir + "/../optopic/preprocessed_datasets/"
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'optopic.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
-
 
 def test_coherence_measures():
     dataset = Dataset()
-    dataset.load(data_dir() + '/m10_validation')
+    dataset.load(data_dir + '/m10_validation')
 
     model = LDA(num_topics=3, iterations=5)
     output = model.train_model(dataset)
