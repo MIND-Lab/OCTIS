244b47d75553ba633200fe51209a3af0481960be
anonymous emnlp2020 <s.terragni4@campus.unimib.it>
2020-12-11 11:13:32 +0100

    defined NMF and NMF_scikit (as separate classes)

--- optopic/models/NMF.py
+++ optopic/models/NMF.py
@@ -1,13 +1,9 @@
 from optopic.models.model import Abstract_Model
 import numpy as np
-from sklearn.decomposition import NMF
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import DictionaryLearning
 from gensim.models import nmf
 import gensim.corpora as corpora
 import optopic.configuration.citations as citations
 import optopic.configuration.defaults as defaults
-import scipy.sparse
 
 
 class NMF(Abstract_Model):
@@ -28,7 +24,7 @@ class NMF(Abstract_Model):
         ----------
         num_topics (int, optional) – Number of topics to extract.
 
-        chunksize (int, optional) – Number of documents to be used in each 
+        chunksize (int, optional) – Number of documents to be used in each
         training chunk.
 
         passes (int, optional) – Number of full passes over the
@@ -50,7 +46,7 @@ class NMF(Abstract_Model):
         w_stop_condition (float, optional) – If error difference gets less
         than that, training of W stops for the current batch.
 
-        h_max_iter (int, optional) – Maximum number of iterations to train 
+        h_max_iter (int, optional) – Maximum number of iterations to train
         h per each batch.
 
         h_stop_condition (float) – If error difference gets less than that,
@@ -232,193 +228,3 @@ class NMF(Abstract_Model):
             for topic_tuple in document:
                 topic_document[topic_tuple[0]][ndoc] = topic_tuple[1]
         return topic_document
-
-
-class NMF_scikit(Abstract_Model):
-
-    id2word = None
-    id_corpus = None
-    use_partitions = True
-    update_with_test = False
-    hyperparameters = {"num_topics": 100,
-                       "init": "random", "alpha": 0, "l1_ratio": 0}
-
-    def __init__(self, num_topics=100, init=None, alpha=0, l1_ratio=0):
-        """
-        Initialize NMF model
-
-        Parameters
-        ----------
-        num_topics (int) – Number of topics to extract.
-
-        init (string, optional) – Method used to initialize the procedure.
-        Default: None. Valid options:
-
-            None: ‘nndsvd’ if n_components <= min(n_samples, n_features),
-            otherwise random.
-
-            ‘random’: non-negative random matrices, scaled with:
-            sqrt(X.mean() / n_components)
-
-            ‘nndsvd’: Nonnegative Double Singular Value Decomposition (NNDSVD)
-            initialization (better for sparseness)
-
-            ‘nndsvda’: NNDSVD with zeros filled with the average of X
-            (better when sparsity is not desired)
-
-            ‘nndsvdar’: NNDSVD with zeros filled with small random values
-            (generally faster, less accurate alternative to NNDSVDa for when 
-            sparsity is not desired)
-
-        alpha (double, optional) – Constant that multiplies the regularization
-        terms. Set it to zero to have no regularization.
-
-        l1_ratio (double, optional) – The regularization mixing parameter, with
-        0 <= l1_ratio <= 1. For l1_ratio = 0 the penalty is an elementwise
-        L2 penalty (aka Frobenius Norm). For l1_ratio = 1 it is an
-        elementwise L1 penalty. For 0 < l1_ratio < 1, the penalty
-        is a combination of L1 and L2.
-        """
-        self.hyperparameters["num_topics"] = num_topics
-        self.hyperparameters["init"] = init
-        self.hyperparameters["alpha"] = alpha
-        self.hyperparameters["l1_ratio"] = l1_ratio
-
-    def hyperparameters_info(self):
-        """
-        Returns hyperparameters informations
-        """
-        return defaults.NMF_scikit_hyperparameters_info
-
-    def partitioning(self, use_partitions, update_with_test=False):
-        """
-        Handle the partitioning system to use and reset the model to perform
-        new evaluations
-
-        Parameters
-        ----------
-        use_partitions: True if train/set partitioning is needed, False
-                        otherwise
-        update_with_test: True if the model should be updated with the test set,
-                          False otherwise
-        """
-        self.use_partitions = use_partitions
-        self.update_with_test = update_with_test
-        self.id2word = None
-        self.id_corpus = None
-
-    def train_model(self, dataset, hyperparameters={}, topics=10,
-                    topic_word_matrix=True, topic_document_matrix=True):
-        """
-        Train the model and return output
-
-        Parameters
-        ----------
-        dataset : dataset to use to build the model
-        hyperparameters : hyperparameters to build the model
-        topics : if greather than 0 returns the most significant words
-                 for each topic in the output
-                 Default True
-        topic_word_matrix : if True returns the topic word matrix in the output
-                            Default True
-        topic_document_matrix : if True returns the topic document
-                                matrix in the output
-                                Default True
-
-        Returns
-        -------
-        result : dictionary with up to 3 entries,
-                 'topics', 'topic-word-matrix' and
-                 'topic-document-matrix'
-        """
-        if self.id2word == None or self.id_corpus == None:
-            vectorizer = TfidfVectorizer(min_df=0.0)
-            corpus = dataset.get_corpus()
-            real_corpus = []
-            for document in corpus:
-                real_corpus.append(" ".join(document))
-            X = vectorizer.fit_transform(real_corpus)
-
-            lista = vectorizer.get_feature_names()
-            self.id2word = {i: lista[i] for i in range(0, len(lista))}
-            if self.use_partitions:
-                ltd = dataset.get_metadata()[
-                    "last-training-doc"]
-                self.id_corpus = X[0:ltd]
-                self.new_corpus = X[ltd:]
-            else:
-                self.id_corpus = X
-
-        hyperparameters["corpus"] = self.id_corpus
-        hyperparameters["id2word"] = self.id2word
-        self.hyperparameters.update(hyperparameters)
-
-        model = NMF(
-            n_components=self.hyperparameters["num_topics"],
-            init=self.hyperparameters["init"],
-            alpha=self.hyperparameters["alpha"],
-            l1_ratio=self.hyperparameters["l1_ratio"])
-
-        W = model.fit_transform(self.id_corpus)
-        #W = W / W.sum(axis=1, keepdims=True)
-        H = model.components_
-        #H = H / H.sum(axis=1, keepdims=True)
-
-        result = {}
-
-        if topic_word_matrix:
-            result["topic-word-matrix"] = H
-
-        if topics > 0:
-            result["topics"] = self.get_topics(H, topics)
-
-        if topic_document_matrix:
-            result["topic-document-matrix"] = np.array(W).transpose()
-
-        if self.use_partitions:
-            if self.update_with_test:
-               # NOT IMPLEMENTED YET
-
-                if topic_word_matrix:
-                    result["test-topic-word-matrix"] = W
-
-                if topics > 0:
-                    result["test-topics"] = self.get_topics(W, topics)
-
-                if topic_document_matrix:
-                    result["test-topic-document-matrix"] = H
-
-            else:
-                result["test-document-topic-matrix"] = model.transform(
-                    self.new_corpus)
-
-        return result
-
-    def get_topics(self, H, topics):
-        topic_list = []
-        for topic in H:
-            words_list = sorted(
-                list(enumerate(topic)), key=lambda x: x[1])
-            topk = [tup[0] for tup in words_list[0:topics]]
-            topic_list.append([self.id2word[i] for i in topk])
-        return topic_list
-
-
-def NMF_Model(implementation="gensim"):
-    """
-    Choose NMF implementation and return the correct model
-
-    Parameters
-    ----------
-    implementation: implementation of NMF to use
-                    available 'gensim' and 'scikit'
-                    default 'gensim'
-
-    Returns
-    -------
-    model: an initialized model of the choosen implementation
-    """
-    if implementation == "gensim":
-        return NMF()
-    if implementation == "scikit":
-        return NMF_scikit()
