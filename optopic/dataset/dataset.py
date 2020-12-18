c74417ba8120a2593b557cb28dc37d9ecec4bec1
Silvia Terragni <s.terragni4@campus.unimib.it>
2020-12-18 10:42:26 +0100

    deep refactoring of dataset.py

--- optopic/dataset/dataset.py
+++ optopic/dataset/dataset.py
@@ -8,8 +8,8 @@ class Dataset:
     access, save and edit the dataset data
     """
 
-    def __init__(self, corpus=[], vocabulary={}, metadata={},
-                 labels=[], edges=[]):
+    def __init__(self, corpus=None, vocabulary=None, metadata=None,
+                 labels=None, edges=None):
         """
         Initialize a dataset, parameters are optional
         if you want to load a dataset, initialize this
@@ -22,22 +22,14 @@ class Dataset:
         labels : labels of the dataset
         edges : edges of the dataset
         """
-        self.set_corpus(corpus)
-        self.set_vocabulary(vocabulary)
-        self.set_metadata(metadata)
-        self.set_labels(labels)
-        self.set_edges(edges)
-
-    # Corpus setter
-
-    def set_corpus(self, corpus):
         self.__corpus = corpus
+        self.__vocabulary = vocabulary
+        self.__metadata = metadata
+        self.__labels = labels
+        self.__edges = edges
 
-    # Whole Corpus getter
     def get_corpus(self):
-        if self.__corpus != []:
-            return self.__corpus
-        return False
+        return self.__corpus
 
     # Partitioned Corpus getter
     def get_partitioned_corpus(self, use_validation=True):
@@ -46,7 +38,7 @@ class Dataset:
         # il validation vuoto
         if use_validation:
             last_validation_doc = self.__metadata["last-validation-doc"]
-            if self.__corpus != [] and last_training_doc != 0:
+            if self.__corpus is not None and last_training_doc != 0:
                 train_corpus = []
                 test_corpus = []
                 validation_corpus = []
@@ -59,7 +51,7 @@ class Dataset:
                     test_corpus.append(self.__corpus[i])
                 return train_corpus, validation_corpus, test_corpus
         else:
-            if self.__corpus != [] and last_training_doc != 0:
+            if self.__corpus is not None and last_training_doc != 0:
                 if "last-validation-doc" in self.__metadata.keys():
                     last_validation_doc = self.__metadata["last-validation-doc"]
                 else:
@@ -78,57 +70,28 @@ class Dataset:
                         test_corpus.append(self.__corpus[i])
                 return train_corpus, test_corpus
 
-
-    # Edges setter
-
-    def set_edges(self, edges):
-        self.__edges = edges
-
     # Edges getter
     def get_edges(self):
-        if self.__edges != []:
-            return self.__edges
-        return False
-
-    # Labels setter
-
-    def set_labels(self, labels):
-        self.__labels = labels
+        return self.__edges
 
     # Labels getter
     def get_labels(self):
-        if self.__labels != []:
-            return self.__labels
-        return False
-
-    # Metadata setter
-
-    def set_metadata(self, metadata):
-        self.__metadata = metadata
+        return self.__labels
 
     # Metadata getter
     def get_metadata(self):
-        if self.__metadata != {}:
-            return self.__metadata
-        return False
+        return self.__metadata
 
     # Info getter
     def get_info(self):
-        if self.__metadata != {}:
-            if "info" in self.__metadata:
-                return self.__metadata["info"]
-        return False
-
-    # Vocabulary setter
-
-    def set_vocabulary(self, vocabulary):
-        self.__vocabulary = vocabulary
+        if "info" in self.__metadata:
+            return self.__metadata["info"]
+        else:
+            return None
 
     # Vocabulary getter
     def get_vocabulary(self):
-        if self.__vocabulary != {}:
-            return self.__vocabulary
-        return False
+        return self.__vocabulary
 
     def _save_metadata(self, file_name):
         """
@@ -138,14 +101,14 @@ class Dataset:
         file_name : name of the file to write
         Returns
         -------
-        True if the data is saved, False otherwise
+        True if the data is saved
         """
         data = self.get_metadata()
-        if data:
+        if data is not None:
             with open(file_name, 'w') as outfile:
                 json.dump(data, outfile)
                 return True
-        return False
+        raise Exception("error in saving metadata")
 
     def _load_metadata(self, file_name):
         """
@@ -153,18 +116,13 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to read
-        Returns
-        -------
-        True if the data is updated, False otherwise
         """
-        metadata = {}
         file = Path(file_name)
         if file.is_file():
             with open(file_name, 'r') as metadata_file:
                 metadata = json.load(metadata_file)
-            self.set_metadata(metadata)
-            return True
-        return False
+            self.__metadata = metadata
+        raise Exception("error in loading metadata")
 
     def _save_corpus(self, file_name):
         """
@@ -172,17 +130,13 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to write
-        Returns
-        -------
-        True if the data is saved, False otherwise
         """
         data = self.get_corpus()
-        if data:
+        if data is not None:
             with open(file_name, 'w') as outfile:
                 for element in data:
                     outfile.write("%s\n" % " ".join(element))
-                return True
-        return False
+        raise Exception("error in saving metadata")
 
     def _load_corpus(self, file_name):
         """
@@ -190,19 +144,14 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to read
-        Returns
-        -------
-        True if the data is updated, False otherwise
         """
-        corpus = []
         file = Path(file_name)
         if file.is_file():
             with open(file_name, 'r') as corpus_file:
-                for line in corpus_file:
-                    corpus.append(line.split())
-            self.set_corpus(corpus)
-            return True
-        return False
+                corpus = [line.strip().split() for line in corpus_file]
+            self.__corpus = corpus
+        else:
+            raise Exception("error in loading corpus")
 
     def _save_edges(self, file_name):
         """
@@ -210,17 +159,13 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to write
-        Returns
-        -------
-        True if the data is saved, False otherwise
         """
         data = self.get_edges()
-        if data:
+        if data is not None:
             with open(file_name, 'w') as outfile:
                 for element in data:
                     outfile.write("%s\n" % element)
-                return True
-        return False
+        raise Exception("error in saving edges")
 
     def _load_edges(self, file_name):
         """
@@ -228,19 +173,14 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to read
-        Returns
-        -------
-        True if the data is updated, False otherwise
         """
-        edges = []
         file = Path(file_name)
         if file.is_file():
             with open(file_name, 'r') as edges_file:
-                for line in edges_file:
-                    edges.append(line[0:len(line)-1])
-            self.set_edges(edges)
-            return True
-        return False
+                edges = [line[0:len(line)-1] for line in edges_file]
+            self.__edges = edges
+        else:
+            raise Exception("error in loading edges")
 
     def _save_labels(self, file_name):
         """
@@ -249,17 +189,13 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to write
-        Returns
-        -------
-        True if the data is saved, False otherwise
         """
         data = self.get_labels()
-        if data:
+        if data is not None:
             with open(file_name, 'w') as outfile:
                 for element in data:
                     outfile.write("%s\n" % json.dumps(element))
-                return True
-        return False
+        raise Exception("error in saving labels")
 
     def _load_labels(self, file_name):
         """
@@ -267,19 +203,14 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to read
-        Returns
-        -------
-        True if the data is updated, False otherwise
+        ----------
         """
-        labels = []
         file = Path(file_name)
         if file.is_file():
             with open(file_name, 'r') as labels_file:
-                for line in labels_file:
-                    labels.append(json.loads(line))
-            self.set_labels(labels)
-            return True
-        return False
+                labels = [json.loads(line.strip()) for line in labels_file]
+            self.__labels = labels
+        raise Exception("error in loading labels")
 
     def _save_vocabulary(self, file_name):
         """
@@ -287,18 +218,15 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to write
-        Returns
         -------
-        True if the data is saved, False otherwise
         """
         data = self.get_vocabulary()
-        if data:
+        if data is not None:
             with open(file_name, 'w') as outfile:
                 for word, freq in data.items():
                     line = word+" "+str(freq)
                     outfile.write("%s\n" % line)
-                return True
-        return False
+        raise Exception("error in saving vocabulary")
 
     def _load_vocabulary(self, file_name):
         """
@@ -306,9 +234,6 @@ class Dataset:
         Parameters
         ----------
         file_name : name of the file to read
-        Returns
-        -------
-        True if the data is updated, False otherwise
         """
         vocabulary = {}
         file = Path(file_name)
@@ -317,9 +242,9 @@ class Dataset:
                 for line in vocabulary_file:
                     tmp = line.split()
                     vocabulary[tmp[0]] = float(tmp[1])
-            self.set_vocabulary(vocabulary)
-            return True
-        return False
+            self.__vocabulary = vocabulary
+        else:
+            raise Exception("error in loading vocabulary")
 
     def save(self, path):
         """
@@ -328,17 +253,16 @@ class Dataset:
         ----------
         path : path to the folder in wich files are saved.
                If the folder doesn't exist it will be created
-        Returns
-        -------
-        True if the data is saved, False otherwise
         """
         Path(path).mkdir(parents=True, exist_ok=True)
-        corpus_saved = self._save_corpus(path+"/corpus.txt")
-        vocabulary_saved = self._save_vocabulary(path+"/vocabulary.txt")
-        self._save_labels(path+"/labels.txt")
-        self._save_edges(path+"/edges.txt")
-        metadata_saved = self._save_metadata(path+"/metadata.json")
-        return corpus_saved and vocabulary_saved and metadata_saved
+        try:
+            self._save_corpus(path+"/corpus.txt")
+            self._save_vocabulary(path+"/vocabulary.txt")
+            self._save_labels(path+"/labels.txt")
+            self._save_edges(path+"/edges.txt")
+            self._save_metadata(path+"/metadata.json")
+        except:
+            raise Exception("error in saving the dataset")
 
     def load(self, path):
         """
@@ -346,16 +270,13 @@ class Dataset:
         Parameters
         ----------
         path : path of the folder to read
-        Returns
-        -------
-        True if the data is saved, False otherwise
         """
-        self.path=path
-        
-        
-        corpus_readed = self._load_corpus(path+"/corpus.txt")
-        vocabulary_readed = self._load_vocabulary(path+"/vocabulary.txt")
-        self._load_labels(path+"/labels.txt")
-        self._load_edges(path+"/edges.txt")
-        metadata_readed = self._load_metadata(path+"/metadata.json")
-        return corpus_readed and vocabulary_readed and metadata_readed
+        try:
+            self.path=path
+            self._load_corpus(path+"/corpus.txt")
+            self._load_vocabulary(path+"/vocabulary.txt")
+            self._load_labels(path+"/labels.txt")
+            self._load_edges(path+"/edges.txt")
+            self._load_metadata(path+"/metadata.json")
+        except:
+            raise Exception("error in loading the dataset")
