2be2dda635e5ec9a5adb4ae9c74b49d53ea2b63c
Silvia Terragni <s.terragni4@campus.unimib.it>
2021-01-15 16:14:26 +0100

    refactoring

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/MIND-LAB/opTopic',
+    url='https://github.com/MIND-LAB/OCTIS',
     version='0.1.0',
     zip_safe=False,
 )
