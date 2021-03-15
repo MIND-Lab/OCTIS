4bc5f4c2eee262f9580d1d7bf5b185199b602e7a
Silvia Terragni <s.terragni4@campus.unimib.it>
2021-03-15 17:52:35 +0100

    fixed early stopping import

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='octis',
     name='octis',
