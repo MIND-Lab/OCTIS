3f3a750f76e4a9d6f726c5a6c143049bb898091e
Silvia Terragni <s.terragni4@campus.unimib.it>
2021-03-15 18:11:40 +0100

    new version

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MIND-LAB/OCTIS',
-    version='0.1.2',
+    version='0.1.2.1',
     zip_safe=False,
 )
