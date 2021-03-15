63ad1926fc9cda6f510d1927a00e6c5d62622050
Silvia <s.terragni4@campus.unimib.it>
2021-03-15 14:23:31 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup_requirements = ['pytest-runner', ]
 
