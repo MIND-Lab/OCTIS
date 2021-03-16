f1467569682a51b7a1f082dcf776dcd650d64e33
Silvia Terragni <s.terragni4@campus.unimib.it>
2021-03-16 10:31:31 +0100

    fix avitm test set

--- octis/models/pytorchavitm/AVITM.py
+++ octis/models/pytorchavitm/AVITM.py
@@ -10,7 +10,7 @@ class AVITM(Abstract_Model):
     def __init__(self, num_topics=10, model_type='prodLDA', activation='softplus',
                  dropout=0.2, learn_priors=True, batch_size=64, lr=2e-3, momentum=0.99,
                  solver='adam', num_epochs=100, reduce_on_plateau=False, prior_mean=0.0,
-                 prior_variance=None, num_layers=2, num_neurons=100):
+                 prior_variance=None, num_layers=2, num_neurons=100, use_partitions=True):
         super(Abstract_Model).__init__()
         self.hyperparameters['num_topics'] = num_topics
         self.hyperparameters['model_type'] = model_type
@@ -29,7 +29,7 @@ class AVITM(Abstract_Model):
         self.hyperparameters["num_layers"] = num_layers
 
         hidden_sizes = tuple([num_neurons for _ in range(num_layers)])
-
+        self.use_partitions = use_partitions
         self.hyperparameters['hidden_sizes'] = tuple(hidden_sizes)
 
     def train_model(self, dataset, hyperparameters=None, top_words=10):
