6f352bfdc5937430a2814856d5242908c0664524
Silvia Terragni <s.terragni4@campus.unimib.it>
2021-03-16 09:14:03 +0100

    added new test ctm

--- tests/test_optopic.py
+++ tests/test_optopic.py
@@ -90,3 +90,29 @@ def test_model_output_etm(data_dir):
     assert type(output['test-topic-document-matrix']) == np.ndarray
     assert output['test-topic-document-matrix'].shape == (num_topics, len(dataset.get_partitioned_corpus()[1]))
 
+
+def test_model_output_ctm_zeroshot(data_dir):
+    dataset = Dataset()
+    dataset.load(data_dir + '/M10')
+    num_topics = 3
+    model = CTM(num_topics=num_topics, num_epochs=5, inference_type='zeroshot')
+    output = model.train_model(dataset)
+    assert 'topics' in output.keys()
+    assert 'topic-word-matrix' in output.keys()
+    assert 'test-topic-document-matrix' in output.keys()
+
+    # check topics format
+    assert type(output['topics']) == list
+    assert len(output['topics']) == num_topics
+
+    # check topic-word-matrix format
+    assert type(output['topic-word-matrix']) == np.ndarray
+    assert output['topic-word-matrix'].shape == (num_topics, len(dataset.get_vocabulary()))
+
+    # check topic-document-matrix format
+    assert type(output['topic-document-matrix']) == np.ndarray
+    assert output['topic-document-matrix'].shape == (num_topics, len(dataset.get_partitioned_corpus()[0]))
+
+    # check test-topic-document-matrix format
+    assert type(output['test-topic-document-matrix']) == np.ndarray
+    assert output['test-topic-document-matrix'].shape == (num_topics, len(dataset.get_partitioned_corpus()[1]))
