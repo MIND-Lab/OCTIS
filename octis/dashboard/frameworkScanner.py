5d644048f2903400c265930df3cba43466e4b7c6
pietrotrope <pi.tropeano@gmail.com>
2021-03-28 16:44:23 +0200

    Comment format updated in dashboard

--- octis/dashboard/frameworkScanner.py
+++ octis/dashboard/frameworkScanner.py
@@ -11,9 +11,8 @@ def scanDatasets():
     """
     Retrieves the name of each dataset present in the framework
 
-    Returns
-    -------
-    res : list with name of each dataset as element
+    :return: list with name of each dataset as element
+    :rtype: List
     """
 
     datasets = os.listdir(str(os.path.join(path, "preprocessed_datasets")))
@@ -25,15 +24,17 @@ def getDatasetMetadata(datasetName):
     """
     Retrieves the dataset metadata
 
-    Returns
-    -------
-    data : dict with metadata if dataset is found, False otherwise
+    :param datasetName: name of the dataset
+    :type datasetName: String
+
+    :return: dict with metadata if dataset is found, False otherwise
+    :rtype: Dict
     """
     file = str(os.path.join(
         path, "preprocessed_datasets", datasetName, "corpus.tsv"))
     if os.path.isfile(file):
         f = open(file,)
-        return {"total_documents":sum(1 for line in f)}
+        return {"total_documents": sum(1 for line in f)}
     return False
 
 
@@ -41,14 +42,14 @@ def getDocPreview(datasetName, documentNumber):
     """
     Retrieve the first 40 words of the selected document
 
-    Parameters
-    ----------
-    datasetName: name of the dataset in which the document is located 
+    :param datasetName: name of the dataset in which the document is located 
                  (the dataset must be in the preprocessed_datasets folder)
-    documentNumber: number of the document to retrieve
+    :type datasetName: String
+    :param documentNumber: number of the document to retrieve
+    :type documentNumber: Int
 
-    Returns
-    out: First 40 words in the document
+    :return: First 40 words in the document
+    :rtype: String
     """
     datasetPath = str(os.path.join(
         path, "preprocessed_datasets", datasetName, "corpus.tsv"))
@@ -64,38 +65,18 @@ def getDocPreview(datasetName, documentNumber):
     return False
 
 
-
-
 def getVocabulary(path):
     """
     Retrieves the vocabulary from the vocabulary file of an ezxperiment
 
-    Returns
-    -------
-    vocabulary : a dictionary with id as a key and word as value,
+    :param path: path of the vocabulary
+    :type path: String
+
+    :return: a dictionary with id as a key and word as value,
                  returns False if the vocabulary is not found
+    :rtype: Dict
     """
     if Path(path).is_file():
         vocabulary_file = open(path,)
         return json.load(vocabulary_file)
     return False
-
-
-#def getVocabulary(path):
-#    """
-#    Retrieves the vocabulary from the vocabulary file of an ezxperiment
-#
-#    Returns
-#    -------
-#    vocabulary : a dictionary with id as a key and word as value,
-#                 returns False if the vocabulary is not found
-#    """
-#    count = 0
-#    vocabulary = {}
-#    if Path(path).is_file():
-#        with open(path, 'r') as f:
-#            for line in f:
-#                vocabulary[str(count)] = line.strip()
-#                count+= 1  
-#        return vocabulary      
-#    return False
