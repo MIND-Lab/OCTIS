bd407b73c373c547e0476b4854dea3d070156bb5
Silvia Terragni <s.terragni4@campus.unimib.it>
2021-05-12 20:57:50 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MIND-LAB/OCTIS',
-    version='version='1.4.0'',
+    version='1.4.0',
     zip_safe=False,
 )
