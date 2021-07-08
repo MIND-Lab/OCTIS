1909dd689acf10ff5078c378c8dbf31bff2400e3
Silvia Terragni <s.terragni4@campus.unimib.it>
2021-07-08 17:43:38 +0200

    Bump version: 1.8.0 → 1.8.1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MIND-LAB/OCTIS',
-    version='1.8.0',
+    version='1.8.1',
     zip_safe=False,
 )
