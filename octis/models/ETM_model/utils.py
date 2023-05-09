2bb3a027f8896692372af1080689267f51bb193f
Silvia Terragni <s.terragni4@campus.unimib.it>
2023-05-09 09:36:07 +0200

    refactoring

--- octis/models/ETM_model/utils.py
+++ octis/models/ETM_model/utils.py
@@ -1,22 +1,23 @@
-import torch 
 import numpy as np
+
 
 def get_topic_diversity(beta, topk):
     num_topics = beta.shape[0]
     list_w = np.zeros((num_topics, topk))
     for k in range(num_topics):
-        idx = beta[k,:].argsort()[-topk:][::-1]
-        list_w[k,:] = idx
+        idx = beta[k, :].argsort()[-topk:][::-1]
+        list_w[k, :] = idx
     n_unique = len(np.unique(list_w))
     TD = n_unique / (topk * num_topics)
     print('Topic diveristy is: {}'.format(TD))
+
 
 def get_document_frequency(data, wi, wj=None):
     if wj is None:
         D_wi = 0
         for l in range(len(data)):
             doc = data[l].squeeze(0)
-            if len(doc) == 1: 
+            if len(doc) == 1:
                 continue
             else:
                 doc = doc.squeeze()
@@ -27,7 +28,7 @@ def get_document_frequency(data, wi, wj=None):
     D_wi_wj = 0
     for l in range(len(data)):
         doc = data[l].squeeze(0)
-        if len(doc) == 1: 
+        if len(doc) == 1:
             doc = [doc.squeeze()]
         else:
             doc = doc.squeeze()
@@ -35,10 +36,11 @@ def get_document_frequency(data, wi, wj=None):
             D_wj += 1
             if wi in doc:
                 D_wi_wj += 1
-    return D_wj, D_wi_wj 
+    return D_wj, D_wi_wj
+
 
 def get_topic_coherence(beta, data, vocab):
-    D = len(data) ## number of docs...data is list of documents
+    D = len(data)  # number of docs...data is list of documents
     print('D: ', D)
     TC = []
     num_topics = len(beta)
@@ -60,21 +62,24 @@ def get_topic_coherence(beta, data, vocab):
                 if D_wi_wj == 0:
                     f_wi_wj = -1
                 else:
-                    f_wi_wj = -1 + ( np.log(D_wi) + np.log(D_wj)  - 2.0 * np.log(D) ) / ( np.log(D_wi_wj) - np.log(D) )
-                # update tmp: 
+                    f_wi_wj = -1 + (
+                        np.log(D_wi) + np.log(D_wj) - 2.0 * np.log(D)) / (
+                        np.log(D_wi_wj) - np.log(D))
+                # update tmp:
                 tmp += f_wi_wj
                 j += 1
                 counter += 1
             # update TC_k
-            TC_k += tmp 
+            TC_k += tmp
         TC.append(TC_k)
     print('counter: ', counter)
     print('num topics: ', len(TC))
     TC = np.mean(TC) / counter
     print('Topic coherence is: {}'.format(TC))
 
+
 def nearest_neighbors(word, embeddings, vocab):
-    vectors = embeddings.data.cpu().numpy() 
+    vectors = embeddings.data.cpu().numpy()
     index = vocab.index(word)
     print('vectors: ', vectors.shape)
     query = vectors[index]
